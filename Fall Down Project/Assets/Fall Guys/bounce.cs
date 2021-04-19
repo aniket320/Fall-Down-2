e40c93f8471b79a11d4165dbbe56f8eafe787eaf
aniket320 <79084583+aniket320@users.noreply.github.com>
2021-04-19 19:24:22 +0530

    hghg
    hghgh

---
+++ Fall Down Project/Assets/Fall Guys/bounce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounce : MonoBehaviour
+{
+
+    public float str = 0.2f;
+    private Rigidbody rb;
+
+
+     void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag=="Bouncer")
+        {
+
+            rb.AddForce(rb.velocity*str,ForceMode.Impulse);
+            //Vector3 direction = (transform.position - other.transform.position).normalized;
+           // rb.velocity = Vector3.forward * speed;
+        }
+    }
+}
---
+++ Fall Down Project/Assets/Fall Guys/touchdoors.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchdoors : MonoBehaviour
+{
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+
+
+            rb.isKinematic = false;
+           
+        }
+    }
+}
