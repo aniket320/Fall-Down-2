7f8fc3143b9bc5ce7a80488777692482adf00a37
aniket320 <79084583+aniket320@users.noreply.github.com>
2021-04-26 12:12:23 +0530

    skin
    skkins changer

---
+++ Fall Down Project/Assets/Fall Guys/color.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class color : MonoBehaviour
+{
+    public FlexibleColorPicker fcp;
+    public Material player;
+   
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+       player.color=fcp.color;
+       
+     
+    }
+   public void save()
+    {
+        PlayerPrefs.Save();
+    }
+   
+}
---
+++ Fall Down Project/Assets/p.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p : MonoBehaviour
+{
+    public Material mat;
+    public FlexibleColorPicker fcp;
+
+    private void Update()
+    {
+        mat.color = fcp.color;
+
+    }
+
+    //save code
+
+    private void OnDisable()
+    {
+        string saveColor = ColorUtility.ToHtmlStringRGB(fcp.color);
+        PlayerPrefs.SetString("MyColorValue", saveColor);
+    }
+
+    //load code
+
+    private void OnEnable()
+    {
+        string saveColor = PlayerPrefs.GetString("MyColorValue");
+        Color color;
+        if (ColorUtility.TryParseHtmlString(saveColor, out color))
+            
+        
+            fcp.color = color;
+        
+    }
+
+
+}
