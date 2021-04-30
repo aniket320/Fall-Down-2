c5b68a74986bda2449499df179bcf0f3c8438b99
aniket320 <79084583+aniket320@users.noreply.github.com>
2021-04-30 14:46:14 +0530

    xxx
    xxxx

---
+++ Fall Down Project/Assets/Fall Guys/Scripts/restart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+    [SerializeField] private Transform respawnpoint;
+
+
+
+
+
+    private void OnCollisionEnter(Collision g)
+    {
+        if(g.gameObject.CompareTag("Player"))
+        {
+            Player.transform.position = respawnpoint.transform.position;
+        }
+    }
+}
